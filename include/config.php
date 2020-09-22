45b86c74ac1d26d563791d697e902f660d5aa3bd
Eric <e.martin@estudiantes.matcom.uh.cu>
2020-09-21 22:04:55 -0400

    Add Testing Config

--- include/config.php
+++ include/config.php
@@ -1,3 +1,5 @@
 <?php 
 if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
-$data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>aWNlbA==');
+$data['mikhmon'] = array ('1'=>'mikhmon<|<Admin','mikhmon>|>qJKgm5mbrKA=');
+
+$data['Admin'] = array ('1'=>'Admin!192.168.88.1','Admin@|@admin','Admin#|#qJKgm5mbrKA=','Admin%Welcome','Admin^welcome.com','Admin&CUC','Admin*10','Admin(1','Admin)','Admin=10','Admin@!@enable');
